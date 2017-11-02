53ada01f0894da17972641be48ecb2e743179899
musitive <dallinfrank@gmail.com>
2017-11-01 19:46:57 -0600

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('weather.html', { root: 'public' });
+  res.sendFile('travel.html', { root: 'public' });
 });
 
 
