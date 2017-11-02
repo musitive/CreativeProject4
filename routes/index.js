08c1f8341c0d6cb6dec90ee79c3bbce5769c9ecb
Ubuntu <dallinfrank@gmail.com>
2017-11-02 00:58:57 +0000

    fdsa

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+const fs = require('fs');
+const request = require('request');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('weather.html', { root: 'public' });
+});
+
+
+router.get('/getcity', function(req, res, next) {
+  var myRe = new RegExp("^" + req.query.q.toLowerCase());
+  fs.readFile(__dirname + '/cities.dat.txt', function(err, data) {
+    if(err) throw err;
+    var cities = data.toString().split('\n');
+	var jsonresult = [];
+    for(var i = 0; i < cities.length; i++) {
+	var result = cities[i].toLowerCase().search(myRe);
+	if (result != -1) {
+	  console.log(cities[i]);
+	  jsonresult.push({city:cities[i]});
+	}
+    }
+	res.status(200).json(jsonresult);
+  })
+});
+
+router.get('/getdef', function(req, res, next) {
+  var word = req.query.w.toLowerCase();
+  http.get('https://owlbot.info/api/v1/dictionary/' + word, function(response) {
+	console.log(response);
+	})
+});
+
+router.get('/getword', function(req,res, next) {
+  var owl = "https://owlbot.info/api/v1/dictionary/";
+  owl += req.query.q;
+  console.log("get word");
+  request(owl).pipe(res);
+});
+
+module.exports = router;
